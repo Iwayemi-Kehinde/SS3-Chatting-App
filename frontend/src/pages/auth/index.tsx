2775329a5306aa73d912d41bf7fac2b5b5d0fde4
Iwayemi-Kehinde <iwayemikehinde1@gmail.com>
2024-07-21 03:42:36 +0900

    added the signin page

--- frontend/src/pages/auth/index.tsx
+++ frontend/src/pages/auth/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import victoryImg from "../../assets/victory.svg";
+import loginBackground from "../../assets/login2.png"
 
 interface FormData {
   email: string;
@@ -13,15 +13,33 @@ const Auth = () => {
     password: "",
   });
 
+  const [isNotSignedIn, setIsNotSignedIn] = React.useState<Boolean>(true)
+
+  const handlePage = () => {
+    setIsNotSignedIn((prev) => !prev)
+  }
+
+
+  const handleChange = (e: any) => {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
   return (
+    isNotSignedIn ? (
     <div className="min-h-[100vh] bg-blue-100 flex justify-center">
+      <img src={loginBackground} alt="img" className="hidden"/>
       <div className="my-20">
         <div className="flex items-center justify-center mb-[-15px]">
           <h2 className="text-gray-800 text-center uppercase">Welcome!</h2>
           <img className="w-20" src={victoryImg} alt="img" />
         </div>
           <span className="text-center block mb-[15px] text-gray-500">Fill in the details to get started</span>
-        <form className="bg-white rounded-lg w-[500px] min-h-[350px] py-6 px-12">
+        <form onSubmit={handleSubmit} className="bg-white rounded-lg w-[500px] h-[max-content] py-6 px-12">
           <div className="mb-[12px]">
             <label className="block text-lg">Email:</label>
             <input
@@ -30,6 +48,7 @@ const Auth = () => {
               className="w-full text-base rounded-sm border border-gray-400 border-solid focus:border-blue-500  p-2 outline-none"
               placeholder="email"
               id="email"
+              onChange={handleChange}
             />
           </div>
 
@@ -40,15 +59,29 @@ const Auth = () => {
               className="w-full text-base rounded-sm border border-gray-400 border-solid focus:border-blue-500  p-2 outline-none"
               placeholder="password"
               id="password"
+              onChange={handleChange}
             />
           </div>
 
+          
+          <div className="mb-[12px]">
+            <label className="block text-lg">confirm-password:</label>
+            <input
+              type="password"
+              className="w-full text-base rounded-sm border border-gray-400 border-solid focus:border-blue-500  p-2 outline-none"
+              placeholder="confirm password"
+              id="confirmPassword"
+              onChange={handleChange}
+            />
+          </div>
+
+
           <div className="mb-[30px]">
             <p className="block text-lg">
-              {"Don't have an account"}{" "}
-              <Link to="/signup" className="text-blue-500 cursor-pointer">
-                Sign up
-              </Link>
+              {"Alreday have an account?"}{" "}
+              <span onClick={handlePage} className="text-blue-500 cursor-pointer">
+                Sign in
+              </span>
             </p>
           </div>
 
@@ -57,13 +90,68 @@ const Auth = () => {
               type="submit"
               className=" w-[100%] cursor-pointer p-1 text-lg rounded-[3px] text-white bg-blue-500 border-0 hover:opacity-90  disabled:opacity-50 transition"
             >
-              {"Login"}
+              {"Sign up"}
             </button>
           </div>
         </form>
       </div>
-    </div>
+      </div>) : (
+         <div className="min-h-[100vh] bg-blue-100 flex justify-center">
+         <img src={loginBackground} alt="img" className="hidden"/>
+         <div className="my-20 h-[max-content] ">
+           <div className="flex items-center justify-center mb-[-15px]">
+             <h2 className="text-gray-800 text-center uppercase">Welcome!</h2>
+             <img className="w-20" src={victoryImg} alt="img" />
+           </div>
+             <span className="text-center block mb-[15px] text-gray-500">Fill in the details to get started</span>
+           <form onSubmit={handleSubmit} className="bg-white rounded-lg w-[500px] h-[max-content]  py-6 px-12">
+             <div className="mb-[12px]">
+               <label className="block text-lg">Email:</label>
+               <input
+                 type="email"
+                 required
+                 className="w-full text-base rounded-sm border border-gray-400 border-solid focus:border-blue-500  p-2 outline-none"
+                 placeholder="email"
+                 id="email"
+                 onChange={handleChange}
+               />
+             </div>
+   
+             <div className="mb-[12px]">
+               <label className="block text-lg">Password:</label>
+               <input
+                 type="password"
+                 className="w-full text-base rounded-sm border border-gray-400 border-solid focus:border-blue-500  p-2 outline-none"
+                 placeholder="password"
+                 id="password"
+                 onChange={handleChange}
+               />
+             </div>
+   
+   
+             <div className="mb-[30px]">
+               <p className="block text-lg">
+                 {"don't have an account?"}{" "}
+                 <span onClick={handlePage} className="text-blue-500 cursor-pointer">
+                   Sign up
+                 </span>
+               </p>
+             </div>
+   
+             <div className="text-center">
+               <button
+                 type="submit"
+                 className=" w-[100%] cursor-pointer p-1 text-lg rounded-[3px] text-white bg-blue-500 border-0 hover:opacity-90  disabled:opacity-50 transition"
+               >
+                 {"Sign in"}
+               </button>
+             </div>
+           </form>
+         </div>
+         </div>
+    )
   );
 };
+
 
 export default Auth;
