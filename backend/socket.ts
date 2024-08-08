e16850dda8273652543323a4f73c3afd7716f96c
Iwayemi-Kehinde <iwayemikehinde1@gmail.com>
2024-08-08 20:59:17 +0100

    socket connection bug fixed

--- backend/socket.ts
+++ backend/socket.ts
@@ -1,4 +1,5 @@
 import { Server as SocketIOServer } from "socket.io"
+import Messages from "./models/MessagesModel"
 
 const setupSocket = (server: any) => {
 	const io = new SocketIOServer(server, {
@@ -15,19 +16,20 @@ const setupSocket = (server: any) => {
 		console.log(`Client disconnected ${socket.id}`)
 		for(const [userId, socketId] of userSocketMap.entries()) {
 			if(socketId === socket.id) {
+				userSocketMap.delete(userId)
 				break;
 			}
 		}
 	}
 
-	const sendMessage = async (message) => {
-		const senderSocketId = userSocketMap.get(message.sender)
-		const recipeintSocketId = userSocketMap.get(message.recipient)
+	const sendMessage = async (message: any) => {
+		const senderSocketId = userSocketMap.get(message.sender)   
+		const recipientSocketId = userSocketMap.get(message.recipient)
 
-		const createMessage = await Message.create(message)
+		const createdMessage = await Messages.create(message)
 
-		const messageData = await MessageChannel.findById(createdMessage._id).populate("sender", "id email firstName lastNsme image color").populate("recipient", "id email firstName lastNsme image color")
-		if(recipeintSocketId) {
+		const messageData = await Messages.findById(createdMessage._id).populate("sender", "id email firstName lastName image color").populate("recipient", "id email firstName lastName image color")
+		if(recipientSocketId) {
 			io.to(recipientSocketId).emit("recieveMessage", messageData)
 		}
 
